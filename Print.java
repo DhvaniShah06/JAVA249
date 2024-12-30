b23c518044da6654dadd42e8ebd8089e22bbdf30
Dhvani Shah <dhvanihshah06@gmail.com>
2024-12-30 16:18:09 +0530

    Print.java

---
+++ Print.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Print
+{
+	public static void main(String[] args)
+	{
+		Scanner P=new Scanner(System.in);
+		System.out.print("Enter Number : ");
+		int a=P.nextInt();
+		int i=1;
+			for(i=1;i<=a;i++){
+				System.out.println(i);
+
+		}
+	}
+}
+	
