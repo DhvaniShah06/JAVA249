9b200030c189c91263c36074a27fddde83fa0ee2
Dhvani Shah <dhvanihshah06@gmail.com>
2024-12-30 16:16:46 +0530

    Practice.java

--- Practice.java
+++ Practice.java
@@ -3,12 +3,36 @@ public class Practice
 {
 	public static void main(String[] args)
 	{
-		int i=1,flag=0;
+		Scanner a=new Scanner(System.in);
+		System.out.print("Enter Initial number : ");
+		int b=a.nextInt();
+		System.out.print("Enter Ending Number : ");
+		int c=a.nextInt();
+		
+		for(;b<=c;b++){
+				if(b%2==0){
+					System.out.print(b);
+					System.out.println(" Is Even");
+		}
+		else{
+			System.out.print(b);
+			System.out.println(" Is Odd");
+		}
+		}
+
+	}
+}
+// For armstrog
+// n==b
+//System.out.print("Enter Number Of Digits : ");
+		//int c=a.nextInt();
+//r=b/10; sum=summ+(r*r*r);b=b/10;
+// if(sum==n)System.out.print("Number is Armstrong") else System.out.print("Number is not armstrong")
+/*int i=1,flag=0;
 		Scanner a=new Scanner(System.in);
 		System.out.print("Enter Number : ");
+		
 		int b=a.nextInt();
-		//System.out.print("Enter Number Of Digits : ");
-		//int c=a.nextInt();
 		for(i=1;i<=b;i++){
 			if(b%i==0){
 				flag=flag+1;
@@ -19,7 +43,4 @@ public class Practice
 		}
 		else{
 			System.out.print("Number Is Not Prime");
-		}
-
-	}
-}
+		}*/
